488bd80bae0457c8588e96f8b30e296654056220
Juan Diego Garcia Escobar <juan.diego2003@hotmail.com>
2023-09-03 19:49:24 -0500

    Initial commit

---
+++ src/main/java/com/farjuce/appreservas/AppreservasApplication.java
@@ -0,0 +1,13 @@
+package com.farjuce.appreservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppreservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppreservasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farjuce/appreservas/AppreservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farjuce.appreservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppreservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
