0d3f62342ed4b31e6fb965522c23ce8694c651dc
cfgm1603 <cesargimo@unisabana.edu.co>
2023-09-29 01:19:20 -0500

    Added Test for Branch Controller

---
+++ src/test/java/com/farjuce/appreservas/integration/BranchControllerTest.java
@@ -0,0 +1,37 @@
+package com.farjuce.appreservas.integration;
+
+import com.farjuce.appreservas.bd.brach.BranchRepository;
+import com.farjuce.appreservas.controller.dto.BranchDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalTime;
+
+
+
+@ActiveProfiles(profiles = "test")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BranchControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    @Test
+    void Given_branch_When_added_Then_create_branch() {
+        BranchDTO branchDTO = new BranchDTO("Juan", "Bogotá", "Sede", LocalTime.of(12,30), LocalTime.of(2,30));
+
+        ResponseEntity<Boolean> statusBranchAdd = restTemplate.postForEntity("/branch/add", branchDTO, Boolean.class);
+
+        Assertions.assertTrue(statusBranchAdd.getBody());
+
+    }
+}
--- src/test/java/com/farjuce/appreservas/integration/CustomerControllerTest.java
+++ src/test/java/com/farjuce/appreservas/integration/CustomerControllerTest.java
@@ -1,15 +1,10 @@
 package com.farjuce.appreservas.integration;
 
 import com.farjuce.appreservas.bd.customer.Customer;
-import com.farjuce.appreservas.bd.customer.CustomerRepository;
-import com.farjuce.appreservas.bd.employee.Employee;
 import com.farjuce.appreservas.controller.dto.CustomerDTO;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -18,7 +13,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -30,13 +24,9 @@ public class CustomerControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Mock
-    private CustomerRepository customerRepository;
-
     @Test
     public void Given_customer_When_added_Then_created() {
 
-        List<String> customerName = null;
         CustomerDTO customerDTO = new CustomerDTO("Juan", "juangares@unisabana.edu.co", 3L);
 
         restTemplate.postForEntity("/customer/add",customerDTO, String.class);
